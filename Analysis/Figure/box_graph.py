8a4916b2d0c5411e285c98d40681fd32e7d501b3
DiNS Project <arnol.lemogue@imt-atlantique.fr>
2023-02-06 15:43:02 +0000

    Replace box_graph.py

--- Analysis/Figure/box_graph.py
+++ Analysis/Figure/box_graph.py
@@ -11,18 +11,27 @@ import plotly.express as px
 
 
 # Set the figure size
-plt.rcParams["figure.figsize"] = [8.00, 3.50]
+plt.rcParams["figure.figsize"] = [8.00, 3.0]
 plt.rcParams["figure.autolayout"] = True
+#plt.rcParams["font.serif"] = "Times New Roman NN"
 
 # Import a Seaborn dataset
 # data = sns.load_dataset('abcok.csv')
 data = pd.read_csv("abcok.csv")
 
 # Create a grouped boxplot
-sns.boxplot(x=data['Length_name_ref(L)'], y=data['Gain(%)'], hue=data['RRs:'])
 
-plt.show()
-plt.savefig("query.pdf") 
+palette = sns.color_palette("muted")
+box = sns.boxplot(x=data['Length_name_ref(L)'], y=data['Gain(%)'], hue=data['rrs'], palette = palette, linewidth=1)
+plt.legend(loc='upper right', title='RRs')
+
+plt.xlabel("Name_ref Length", fontname='Times New Roman NN')
+plt.ylabel("% of Gain")
+plt.grid()
+
+
+#plt.show()
+plt.savefig("box_1.pdf") 
 
 
 
